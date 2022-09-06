da8b8634a9e3e89c206f41d6dc93625b93fee70c
bbinc1 <pashigorev.ilya@gmail.com>
2022-09-06 22:04:06 +0300

    FirstStep

---
+++ Assets/Scripts/SaveSystem.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using UnityEngine;
+
+namespace Idle
+{
+    public class SaveSystem : MonoBehaviour
+    {
+        private const string SAVE_KEY = "GameData";
+        
+        private GameData _gameData;
+        public GameData GameData => _gameData;
+
+        public void Initialize()
+        {
+            if (PlayerPrefs.HasKey(SAVE_KEY))
+            {
+                LoadData();
+            }
+            else
+            {
+                _gameData = new GameData();
+            }
+        }
+
+        public void LoadData()
+        {
+            string data = JsonUtility.GetString(SAVE_KEY);
+            _gameData = JsonUtility.FromJson<GameData>(data);
+        }
+
+        public void SaveData()
+        {
+           string json= JsonUtility.ToJson(_gameData);
+           PlayerPrefs.SetString(SAVE_KEY, json);
+        }
+    }
+
+    [System.Serializable]
+    public class GameData
+    {
+        public float Money = 60;
+
+        public void UpdateMoney(float value)
+        {
+            Money = value;
+        }
+    }
+}
