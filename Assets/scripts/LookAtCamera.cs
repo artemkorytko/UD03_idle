a613f1ca1149aaddd101f6cb181ad7408efc296e
Pavel Nikitin <nikitinps92@gmail.com>
2022-09-06 22:04:21 +0300

    1st IDLE lesson

---
+++ Assets/scripts/LookAtCamera.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using UnityEngine;
+
+namespace DefaultNamespace
+{
+    public class LookAtCamera : MonoBehaviour
+    {
+        private void Start()
+        {
+            Vector3 cameraPosition = Camera.main.transform.position;
+            Vector3 lookDirection = (transform.position - cameraPosition).normalized;
+            transform.rotation = Quaternion.LookRotation(lookDirection, transform.up);
+        }
+    }
+}
---
+++ Assets/scripts/SaveSystem.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+using UnityEngine;
+
+namespace DefaultNamespace
+{
+    public class SaveSystem : MonoBehaviour
+    {
+        private GameData _gameData;
+        private const string SAVE_KEY = "GameData";
+        
+        public GameData GameData => _gameData;
+
+        public void Initialize()
+        {
+            if (PlayerPrefs.HasKey(SAVE_KEY))
+            {
+                LoadData();
+            }
+            else
+            {
+                _gameData = new GameData();
+            }
+        }
+
+        public void LoadData()
+        {
+            string data = PlayerPrefs.GetString(SAVE_KEY);
+            _gameData = JsonUtility.FromJson<GameData>(data);
+        }
+
+        public void SaveData()
+        {
+            string json = JsonUtility.ToJson(_gameData);
+            PlayerPrefs.SetString(SAVE_KEY, json);
+        }
+    }
+
+    [Serializable]
+    public class GameData
+    {
+        public  float Money = 60;
+
+        public void UpdateMoney(float value)
+        {
+            Money = value;
+        }
+    }
+}
